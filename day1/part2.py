1b439d8ea6a80f5b4d15f256569b333fc915f1cc
Anastázie <anastazie.riskova@gmail.com>
2021-12-02 00:46:20 +0100

    fixed, still a mess

--- day1/part2.py
+++ day1/part2.py
@@ -1,14 +1,14 @@
 nums = []
 f = open("input.txt")
 for line in f:
-    nums.append(list(map(int, line.split())))
+    nums.append(int(line))
 vysledek = 0
 
 for i in range(len(nums)-3):
-    a = int(nums[i][0])
-    b = int(nums[i + 1][0])
-    c = int(nums[i + 2][0])
-    d = int(nums[i + 3][0])
+    a = int(nums[i])
+    b = int(nums[i + 1])
+    c = int(nums[i + 2])
+    d = int(nums[i + 3])
     if (a+b+c) < (b+c+d):
         vysledek += 1
 
