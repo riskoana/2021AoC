536ffd03dba0f3e1d5dcb95a735fa239bbc902b1
Anastázie <anastazie.riskova@gmail.com>
2021-12-03 23:15:13 +0100

    this is fine

---
+++ day3/part1.py
@@ -0,0 +1,21 @@
+code = ""
+f = open("input.txt")
+for line in f:
+    code += (line.replace("\n", ""))
+
+ones = [0 for x in range(12)]
+
+for i in range(len(code)):
+    if code[i] == "1":
+        ones[i%12] += 1
+
+
+a = 0
+b = 0
+for i in range(len(ones)):
+    if ones[i] > 500:
+        a += 2**(11-i)
+    else:
+        b += 2**(11-i)
+
+print(a*b)
